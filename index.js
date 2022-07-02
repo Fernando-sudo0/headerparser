169a05e5475c7c0945e3631239f6ae31e8f2b4ec
Fernando Gutierrez <fernando.gtz.escarrega@gmail.com>
2022-07-01 20:48:23 -0700

    all was in req.header

--- index.js
+++ index.js
@@ -18,7 +18,12 @@ app.use(express.static('public'));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
-
+app.get("/api/whoami", function(req, res){
+ res.json(
+{"ipaddress":req.headers['host'] ,
+ "language":req.headers['accept-language'], 
+ "software" :req.headers['user-agent']});
+});
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
